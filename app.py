41367f1d14e9e6f26ff23d9fec16998345e4c366
AveryBau <averya34@nycstudents.net>
2026-02-05 09:37:59 -0500

    m

---
+++ app.py
@@ -0,0 +1,37 @@
+import turtle
+from turtle import *
+t = Turtle()
+
+""" t.forward(200) """
+
+""" def square(x):
+    t.forward(x)
+    t.left(90)
+    t.forward(x)
+    t.left(90)
+    t.forward(x)
+    t.left(90)
+    t.forward(x)
+    t.left(90)
+square(200) """
+
+
+""" def rectangle():
+    t.forward(125)
+    t.left(90)
+    t.forward(100)
+    t.left(90)
+    t.forward(125)
+    t.left(90)
+    t.forward(100)
+rectangle() """
+
+def triangle(x):
+    t.forward(x)
+    t.left(120)
+    t.forward(x)
+    t.left(120)
+    t.forward(x)
+triangle(90)
+
+turtle.done()
